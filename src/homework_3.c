c05bc1eec406d46f70ce951279233bc3a42085a5
Dmitrii Motuzov <dimamotuzov07@gmail.com>
2024-10-13 12:11:09 +0300

    moved the reading function to the writing function because I misunderstood the task

--- src/homework_3.c
+++ src/homework_3.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
-//#include "my_test.h"
-//uint8_t
-int write_message(FILE* stream, const void *buf, size_t nbyte){
-	int count_of_bytes=0;
-	for(int i=0;i<(int)nbyte;i++){
-		uint8_t byte;
-		byte=((uint8_t *)buf)[i];
-		if (putc(byte,stream)==EOF){
-			fprintf(stderr,"Write error!");
-			return EOF;
-		}
-		count_of_bytes++;
-	}
-	return count_of_bytes;
-}
+#include "my_test.h"
 
-int read_message(FILE *stream, void *buf){
+int write_message(FILE* stream, const void *buf, size_t nbyte){
 	uint8_t byte;
 	uint8_t new_byte=0;
 	uint8_t drawn_number=0;
 	int count_of_one=0;
 	int count_of_bytes=0;
-	((uint8_t *)buf)[count_of_bytes]=0x7E;
+	putc(0x7E,stream);
 	count_of_bytes++;
 	int count_drawn_numbers=0;
-	while(!feof(stream)){
-		byte=getc(stream);
-		if(byte==0xff){
-			continue;
-		}
+	for (int k=0;k<(int)nbyte;k++){
+		byte=((uint8_t *)buf)[k];
 		new_byte=0;
 		int i=0;
 		if (count_drawn_numbers==8){
-			((uint8_t *)buf)[count_of_bytes]=drawn_number;
+			putc(drawn_number,stream);
 			count_of_bytes++;
 			drawn_number=0;
 			count_drawn_numbers=0;
@@ -64,17 +47,17 @@ int read_message(FILE *stream, void *buf){
 			}
 			new_byte=new_byte | (byte & ((int)pow(2,(7-j))));
 		}
-		((uint8_t *)buf)[count_of_bytes]=new_byte;
+		putc(new_byte,stream);
 		count_of_bytes++;
 	}
 	if (count_drawn_numbers!=0){
-		((uint8_t *)buf)[count_of_bytes]= drawn_number | (0x7E>>count_drawn_numbers);
+		putc(drawn_number | (0x7E>>count_drawn_numbers),stream);
 		count_of_bytes++;
-		((uint8_t *)buf)[count_of_bytes]=(0x7E<<(8-count_drawn_numbers)) | (((int)(pow(2,8-count_drawn_numbers)))-1);
+		putc((0x7E<<(8-count_drawn_numbers)) | (((int)(pow(2,8-count_drawn_numbers)))-1),stream);
 		count_of_bytes++;
 	}
 	else{
-		((uint8_t *)buf)[count_of_bytes]=0x7E;
+		putc(0x7E,stream);
 		count_of_bytes++;
 	}
 	return count_of_bytes;
