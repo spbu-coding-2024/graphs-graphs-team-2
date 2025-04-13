c7e480a26e1b2806cd41384d159c56530c1621e1
Semyon <semen.rosin1@gmail.com>
2025-04-13 14:01:44 +0300

    Change: greeting screen to dark theme

--- src/main/kotlin/view/GreetingView.kt
+++ src/main/kotlin/view/GreetingView.kt
@@ -1,5 +1,6 @@
 package view
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,11 +10,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import CoolColors
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextGeometricTransform
 import androidx.compose.ui.unit.sp
-
 
 enum class FileSystem {
     Json,
@@ -21,19 +22,23 @@ enum class FileSystem {
     Neo4j,
 }
 
-
 @Composable
 fun GreetingView() {
     var fileSystem: FileSystem? = null
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = CoolColors.DarkGray),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            "Choose your file system", fontSize = 100.sp,
+            "Choose your file system",
+            fontSize = 95.sp,
             style = TextStyle(textGeometricTransform = TextGeometricTransform(0.3f, 0.3f)),
-            modifier = Modifier.padding(20.dp)
+            modifier = Modifier.padding(20.dp),
+            color = CoolColors.DarkPurple,
+            fontFamily = FontFamily.Monospace,
         )
         Row(
             horizontalArrangement = Arrangement.spacedBy(10.dp),
@@ -41,44 +46,47 @@ fun GreetingView() {
         ) {
             Button(
                 colors = ButtonDefaults.textButtonColors(
-                    backgroundColor = Color.Black,
-                    contentColor = Color.White
+                    backgroundColor = CoolColors.Purple,
+                    contentColor = CoolColors.Gray,
                 ),
-                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.27f),
+                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.24f),
                 onClick = { fileSystem = FileSystem.Json }) {
                 Text(
                     "Json",
                     style = TextStyle(textGeometricTransform = TextGeometricTransform(0.3f, 0.3f)),
-                    fontSize = 90.sp,
-                    modifier = Modifier.padding(10.dp)
+                    fontSize = 75.sp,
+                    modifier = Modifier.padding(10.dp),
+                    fontFamily = FontFamily.Monospace,
                 )
             }
             Button(
                 colors = ButtonDefaults.textButtonColors(
-                    backgroundColor = Color.Black,
-                    contentColor = Color.White
+                    backgroundColor = CoolColors.Purple,
+                    contentColor = CoolColors.Gray,
                 ),
-                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.4f),
+                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.36f),
                 onClick = { fileSystem = FileSystem.SQLite }) {
                 Text(
                     "SQLite",
                     style = TextStyle(textGeometricTransform = TextGeometricTransform(0.3f, 0.3f)),
-                    fontSize = 90.sp,
-                    modifier = Modifier.padding(10.dp)
+                    fontSize = 75.sp,
+                    modifier = Modifier.padding(10.dp),
+                    fontFamily = FontFamily.Monospace,
                 )
             }
             Button(
                 colors = ButtonDefaults.textButtonColors(
-                    backgroundColor = Color.Black,
-                    contentColor = Color.White
+                    backgroundColor = CoolColors.Purple,
+                    contentColor = CoolColors.Gray,
                 ),
-                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.33f),
+                modifier = Modifier.clip(shape = RoundedCornerShape(35.dp)).weight(0.3f),
                 onClick = { fileSystem = FileSystem.Neo4j }) {
                 Text(
                     "Neo4j",
                     style = TextStyle(textGeometricTransform = TextGeometricTransform(0.3f, 0.3f)),
-                    fontSize = 90.sp,
-                    modifier = Modifier.padding(10.dp)
+                    fontSize = 75.sp,
+                    modifier = Modifier.padding(10.dp),
+                    fontFamily = FontFamily.Monospace
                 )
             }
         }
