ce8fa221f14d2dc02edc8d1de2289ca7741a4862
Semyon <semen.rosin1@gmail.com>
2025-04-13 14:01:02 +0300

    Add: colors

---
+++ src/main/kotlin/CoolColors.kt
@@ -0,0 +1,16 @@
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.graphics.Color
+
+object CoolColors {
+    @Stable
+    val Gray = Color(29, 29, 38)
+
+    @Stable
+    val DarkGray = Color(44, 44, 59)
+
+    @Stable
+    val Purple = Color(140, 110, 185)
+
+    @Stable
+    val DarkPurple = Color(130, 100, 175)
+}
