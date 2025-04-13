be8b4295aa50ca23bf1b2318a528a9b4ea5a7671
Semyon <semen.rosin1@gmail.com>
2025-04-13 14:02:14 +0300

    Add: view model for graph

--- src/main/kotlin/viewModel/GraphViewModel.kt
+++ src/main/kotlin/viewModel/GraphViewModel.kt
@@ -1,18 +1,28 @@
 package viewModel
 
 import androidx.compose.runtime.State
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import model.Graph
+import model.Vertex
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-class GraphViewModel<V, E>(
+class GraphViewModel<V, E> (
     private val graph: Graph<V, E>,
+    private val placement: Map<Vertex<V>, Pair<Dp, Dp>?>,
     showVerticesLabels: State<Boolean>,
     showEdgesWeights: State<Boolean>,
     showEdgesDirections: State<Boolean>,
 ) {
     private val _vertices = graph.vertices.associateWith { v ->
-        VertexViewModel(0.dp, 0.dp, Color.Gray, v, showVerticesLabels)
+        VertexViewModel(
+            placement[v]?.first ?: Random.nextInt(0..800).dp,
+            placement[v]?.first ?: Random.nextInt(0..600).dp,
+            CoolColors.DarkPurple,
+            v,
+            showVerticesLabels,
+        )
     }
     private val _edges = graph.edges.associateWith { e ->
         val fst = _vertices[e.vertices.first]
