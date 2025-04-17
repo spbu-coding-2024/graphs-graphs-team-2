d8d27c6112bcb34ce08e20c4ad3b990b5ff71a52
Semyon <semen.rosin1@gmail.com>
2025-04-17 21:17:15 +0300

    Add: internal getter for IDs and graph type in its view model

--- src/main/kotlin/model/AbstractEdge.kt
+++ src/main/kotlin/model/AbstractEdge.kt
@@ -1,8 +1,8 @@
 package model
 
 interface AbstractEdge {
-    var id: Long
-    var label: String
+    val id: Long
+    val label: String
     val vertices: Pair<AbstractVertex, AbstractVertex>
     val weight: Float
 }
--- src/main/kotlin/model/Graph.kt
+++ src/main/kotlin/model/Graph.kt
@@ -1,8 +1,8 @@
 package model
 
 class Graph (
-    val direction: Boolean = false,
-    val weight: Boolean = false,
+    private val direction: Boolean = false,
+    private val weight: Boolean = false,
 ) : AbstractGraph {
 
     private val _vertices = hashMapOf<Long, Vertex>()
@@ -13,9 +13,9 @@ class Graph (
     override val edges: Collection<AbstractEdge>
         get() = _edges.values
 
-    val isDirected: Boolean
+    override val isDirected: Boolean
         get() = direction
-    val isWeighted: Boolean
+    override val isWeighted: Boolean
         get() = weight
 
     override fun addVertex(id: Long, label: String): AbstractVertex = _vertices.getOrPut(id) { Vertex(label, id) }
@@ -27,7 +27,7 @@ class Graph (
         val first = _vertices[firstID]
         val second = _vertices[secondID]
         if (first == null || second == null)
-            throw IllegalStateException("In graph no vertices with ID $firstID and $secondID")
+            throw IllegalStateException("Graph has no vertices with ID $firstID and $secondID")
 
         return _edges.getOrPut(edgeID) { Edge(edgeLabel, edgeID, first, second, weight) }
     }
@@ -35,8 +35,8 @@ class Graph (
     private data class Vertex(override var label: String, override var id: Long) : AbstractVertex
 
     private data class Edge(
-        override var label: String,
-        override var id: Long,
+        override val label: String,
+        override val id: Long,
         var first: Vertex,
         var second: Vertex,
         override val weight: Float,
