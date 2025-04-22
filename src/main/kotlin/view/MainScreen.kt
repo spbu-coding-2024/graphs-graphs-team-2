6a51ccdbe362ee30f36bf7e5ed90289860768f37
Semyon <semen.rosin1@gmail.com>
2025-04-22 15:12:06 +0300

    Add main screen view

---
+++ src/main/kotlin/view/MainScreen.kt
@@ -0,0 +1,49 @@
+package view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import view.components.CoolColors
+import view.graph.GraphView
+import viewModel.MainScreenViewModel
+
+@Composable
+fun MainScreen(viewModel: MainScreenViewModel) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.background(CoolColors.DarkGray)
+    ) {
+        Column(modifier = Modifier.width(370.dp).background(CoolColors.DarkGray)) {
+            Row {
+                Checkbox(checked = viewModel.showVerticesLabels.value, onCheckedChange = {
+                    viewModel.showVerticesLabels.value = it
+                })
+                Text("Show vertices labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp), color = CoolColors.Purple)
+            }
+            Row {
+                Checkbox(checked = viewModel.showEdgesLabels.value, onCheckedChange = {
+                    viewModel.showEdgesLabels.value = it
+                })
+                Text("Show edges labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp), color = CoolColors.Purple)
+            }
+        }
+
+        Surface(
+            modifier = Modifier
+                .weight(1f),
+            color = CoolColors.Gray,
+        ) {
+            GraphView(viewModel.graphViewModel)
+        }
+    }
+}
