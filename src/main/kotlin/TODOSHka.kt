d8089197abc5be09c1e83681877c3482a5d8fef5
talubik <rustem149849@gmail.com>
2025-04-23 16:23:48 +0300

    delete: Trash files

--- src/main/kotlin/TODOSHka.kt
+++
@@ -1,7 +0,0 @@
-fun todoska(){
-    TODO("add search screen")
-    TODO("add test for konverter")
-    TODO("IMPROVE search screen")
-    TODO("Add algoritms test")
-    TODO("add layout algoritm")
-}
