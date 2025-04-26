1ad5aad4ca315525dcb92cbc7dd3c0b39f809922
dmitriimotuzov <dimamotuzov07@gmail.com>
2025-04-26 14:46:15 +0300

    delete : all c files

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-BASEDIR=$(realpath "$(dirname "$0")")
-ROOTDIR=$(realpath "$BASEDIR/..")
-
-cmake -S . -B "$ROOTDIR/build"
-cmake --build "$ROOTDIR/build"
--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-BASEDIR=$(realpath "$(dirname "$0")")
-ROOTDIR=$(realpath "$BASEDIR/..")
-
-cd "$ROOTDIR/build"
-ctest --verbose
