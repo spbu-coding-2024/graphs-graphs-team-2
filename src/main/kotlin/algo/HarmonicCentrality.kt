27295dd64ecfd2f321475d244b9b19e1b4b16af6
Росин Семён <semen.rosin1@gmail.com>
2025-04-27 22:44:37 +0300

    Semrosin work (#27)
    Fix code format(

--- src/main/kotlin/algo/HarmonicCentrality.kt
+++ src/main/kotlin/algo/HarmonicCentrality.kt
@@ -6,11 +6,11 @@ class HarmonicCentrality(private val graph: Graph) {
     fun getVertexCentrality(u: Long): Float {
         var sum = 0f
         graph.vertices.onEach {
-            if(it.id != u) {
+            if (it.id != u) {
                 val path = AlgoDijkstra(graph, u, it.id)
                 path.dijkstra(u)
                 val pathWeight = path.weightMinWay
-                if(pathWeight != null) sum += 1 / pathWeight
+                if (pathWeight != null) sum += 1 / pathWeight
             }
         }
         return sum
