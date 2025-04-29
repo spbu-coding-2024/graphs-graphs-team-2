80e3b941d7e73e5d2408838052d52d612ba6c7c2
dmitriimotuzov <dimamotuzov07@gmail.com>
2025-04-29 17:25:42 +0300

    format : code style

--- src/main/kotlin/algo/Dijkstra.kt
+++ src/main/kotlin/algo/Dijkstra.kt
@@ -2,16 +2,19 @@ package algo
 
 import model.Graph
 
-
 class AlgoDijkstra(val graph: Graph, val firstVertexId: Long, val secondVertexId: Long) {
     private val infinity = 1_000_000_000_000_000_000F
     private val distance =
-        graph.vertices.associate { it.id to if (it.id == firstVertexId) 0F else infinity }.toMutableMap()
-    private val labels = graph.vertices.associate { it.id to (it.id == firstVertexId) }.toMutableMap()
-    private val parents = graph.vertices.associate { it.id to -1L }.toMutableMap()
-    private val graphMap = graph.graphWeightedMap
+        graph.vertices
+            .associate { it.id to if (it.id == firstVertexId) 0F else infinity }
+            .toMutableMap()
+    private val labels =
+        graph.vertices.associate { it.id to (it.id == firstVertexId) }.toMutableMap()
+    private val parents = graph.vertices.associate { it.id to null as Long? }.toMutableMap()
+    private val graphMap = graph.weightedMap
     val way = ArrayDeque<Long>()
-    var weightMinWay : Float? = null
+
+    var weightMinWay: Float? = null
 
     fun dijkstra(Vid: Long) {
         val edges = graphMap[Vid]
@@ -26,9 +29,10 @@ class AlgoDijkstra(val graph: Graph, val firstVertexId: Long, val secondVertexId
             }
         }
 
-
         var minDistance = infinity
+
         var new_Vid: Long? = null
+
         for (i in labels) {
             if (!i.value) {
                 if (distance[i.key]!! < minDistance) {
@@ -37,17 +41,18 @@ class AlgoDijkstra(val graph: Graph, val firstVertexId: Long, val secondVertexId
                 }
             }
         }
+
         if (new_Vid != null) {
             labels[new_Vid] = true
             if (new_Vid == secondVertexId) {
                 weightMinWay = minDistance
+
                 findMinWay(new_Vid)
             } else {
                 dijkstra(new_Vid)
             }
         }
     }
-
 
     private fun findMinWay(V: Long) {
         way.addFirst(V)
