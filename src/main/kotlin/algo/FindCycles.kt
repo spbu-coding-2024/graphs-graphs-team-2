19f8751f5ce7bbe9206a63a28a12fcf31f555cbc
talubik <rustem149849@gmail.com>
2025-04-30 10:33:44 +0300

    Add : find loop algorithm

--- src/main/kotlin/algo/FindCycles.kt
+++ src/main/kotlin/algo/FindCycles.kt
@@ -1,31 +1,26 @@
 package algo
 
-import io.VertexInfo
 import model.Graph
 
-class FindLoops (graph: Graph, val vertex: Long) {
-    private val isVisited =
-        graph.vertices.associate { it.id to false }.toMutableMap()
-    private val graph = graph.weightedMap
+class FindLoops(val graph: Graph, val vertex: Long) {
+    private val isVisited = graph.vertices.associate { it.id to false }.toMutableMap()
+    private val graphMap = graph.weightedMap
     private val parents = mutableMapOf<Long, Long>()
-    val loopEdges=ArrayDeque<Long>()
-    fun callByGraphDirection(){
+    val loopEdges = ArrayDeque<Long>()
 
-    }
-
-    fun findLoopInDirectedGraph(){
+    fun findLoopInDirectedGraph() {
         loopEdges.addLast(vertex)
         outer@ while (!loopEdges.isEmpty()) {
             val vert = loopEdges.first()
-            isVisited[vert]=true
-            val edges= graph[vert]
+            isVisited[vert] = true
+            val edges = graphMap[vert]
 
             if (edges != null) {
                 for (el in edges) {
-                    if(isVisited[el.first]==false) {
+                    if (isVisited[el.first] == false) {
                         loopEdges.addFirst(el.first)
                         continue@outer
-                    }else if(el.first==vertex){
+                    } else if (el.first == vertex) {
                         loopEdges.addFirst(vertex)
                         return
                     }
@@ -33,23 +28,24 @@ class FindLoops (graph: Graph, val vertex: Long) {
             }
             loopEdges.removeFirst()
         }
+        loopEdges.clear()
     }
 
-    fun findLoopInUndirectedGraph(){
+    fun findLoopInUndirectedGraph() {
         val parents = mutableMapOf<Long, Long>()
         loopEdges.addFirst(vertex)
         outer@ while (!loopEdges.isEmpty()) {
             val vert = loopEdges.first()
-            isVisited[vert]=true
-            val edges= graph[vert]
+            isVisited[vert] = true
+            val edges = graphMap[vert]
 
             if (edges != null) {
                 for (el in edges) {
-                    if(isVisited[el.first]==false) {
-                        parents[el.first]=vert
+                    if (isVisited[el.first] == false) {
+                        parents[el.first] = vert
                         loopEdges.addFirst(el.first)
                         continue@outer
-                    }else if(el.first==vertex && parents[vert]!=vertex){
+                    } else if (el.first == vertex && parents[vert] != vertex) {
                         loopEdges.addFirst(vertex)
                         return
                     }
@@ -57,5 +53,6 @@ class FindLoops (graph: Graph, val vertex: Long) {
             }
             loopEdges.removeFirst()
         }
+        loopEdges.clear()
     }
 }
