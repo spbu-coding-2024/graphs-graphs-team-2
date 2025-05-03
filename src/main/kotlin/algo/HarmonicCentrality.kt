f43008362f8c697eef1949cdde1417290991a30b
Semyon <semen.rosin1@gmail.com>
2025-05-03 18:59:31 +0300

    Fix: harmonic centrality (for weigthed graph)

--- src/main/kotlin/algo/HarmonicCentrality.kt
+++ src/main/kotlin/algo/HarmonicCentrality.kt
@@ -9,8 +9,8 @@ class HarmonicCentrality(private val graph: Graph) {
             if (it.id != u) {
                 val path = AlgoDijkstra(graph, u, it.id)
                 path.dijkstra(u)
-                val pathWeight = path.weightMinWay
-                if (pathWeight != null) sum += 1 / pathWeight
+                val pathLenght = path.way.size
+                if (pathLenght != 0) sum += 1 / (pathLenght - 1)
             }
         }
         return sum
