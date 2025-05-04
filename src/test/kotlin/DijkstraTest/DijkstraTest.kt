37fc5385eb4d0210190acddaed4f3445c8ff7b98
dmitriimotuzov <dimamotuzov07@gmail.com>
2025-05-05 02:18:37 +0300

    add : graph generator for dijkstra test

---
+++ src/test/kotlin/DijkstraTest/DijkstraTest.kt
@@ -0,0 +1,90 @@
+package DijkstraTest
+
+import model.Graph
+import java.util.stream.Stream
+import org.junit.jupiter.params.provider.Arguments
+import kotlin.math.min
+import kotlin.random.Random
+
+class DijkstraTest {
+    private val infinity = 1_000_000_000_000_000_000F
+
+    fun graphGenerator(): Stream<Arguments> {
+        return Stream.generate {
+            //vertex ids 0..100
+
+            val calculateEdgeId = { firstVertexId: Int, secondVertexId: Int ->
+                firstVertexId * 1000 + secondVertexId
+            }
+
+            val weightsOfEdges = mutableMapOf<Int, Int>()
+
+            val graph = Graph(true, true)
+            val start = Random.nextInt(0, 100)
+            graph.addVertex(start.toLong(), "")
+            val end = Random.nextInt(0, 100)
+            graph.addVertex(end.toLong(), "")
+
+            val minWaysWeights = Array(101) { if (it == start) 0F else infinity }
+            val parent = Array(101) { infinity.toInt() }
+            val countOfWays = Random.nextInt(5, 20)
+            val maxWeight = 5000
+
+            for (i in 0..countOfWays) {
+                var totalWeight = 0F
+                var oldVertex = start
+                do {
+                    val newVertex = Random.nextInt(0, 100)
+                    graph.addVertex(newVertex.toLong(), "")
+                    var weight = Random.nextInt(1, 1000)
+                    val edge = graph.addEdge(
+                        oldVertex.toLong(),
+                        newVertex.toLong(),
+                        "",
+                        calculateEdgeId(oldVertex, newVertex).toLong(),
+                        weight.toFloat()
+                    )
+                    weight = edge.weight.toInt()
+                    weightsOfEdges.put(calculateEdgeId(oldVertex, newVertex), weight)
+                    totalWeight += weight
+
+                    if (totalWeight < minWaysWeights[newVertex]) {
+                        minWaysWeights[newVertex] = totalWeight
+                        parent[newVertex] = oldVertex
+                        var currentVertex = newVertex
+                        val vertexForChangeWeight = ArrayDeque<Int>()
+                        do {
+                            graph.vertices.forEach { vertex ->
+                                if (parent[vertex.id.toInt()] == currentVertex) {
+                                    if (minWaysWeights[currentVertex] + (weightsOfEdges.get(
+                                            calculateEdgeId(
+                                                currentVertex,
+                                                vertex.id.toInt()
+                                            )
+                                        ) ?: infinity.toInt()) < minWaysWeights[vertex.id.toInt()]
+                                    ) {
+                                        minWaysWeights[vertex.id.toInt()] =
+                                            minWaysWeights[currentVertex] + (weightsOfEdges.get(
+                                                calculateEdgeId(
+                                                    currentVertex,
+                                                    vertex.id.toInt()
+                                                )
+                                            ) ?: infinity.toInt())
+                                        vertexForChangeWeight.add(vertex.id.toInt())
+                                    }
+                                }
+                            }
+                            vertexForChangeWeight.remove(currentVertex)
+                            currentVertex = vertexForChangeWeight.get(0)
+                        } while (vertexForChangeWeight.size != 0)
+                    }
+                    else{
+                        totalWeight = minWaysWeights[newVertex]
+                    }
+                    oldVertex = newVertex
+                } while (totalWeight < maxWeight)
+            }
+            Arguments.of(graph,parent)
+        }.limit(10)
+    }
+}
